7ce007150dd8612bdec129ba3b27c50f05950ad8
victorcodebase <victorkithinji@outlook.com>
2025-07-31 13:41:54 +0300

    next config file path update.. again

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
+// next.config.mjs
 import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	webpack: (config) => {
-		config.resolve.alias["@"] = path.resolve("./src");
+		config.resolve.alias["@"] = path.join(__dirname, "src");
 		return config;
 	},
 };

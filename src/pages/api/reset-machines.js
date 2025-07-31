2f5bf27856ed98ca6f21f6723466410c3a1bbc2d
victorcodebase <victorkithinji@outlook.com>
2025-07-31 13:43:31 +0300

    axed problematic file

--- src/pages/api/reset-machines.js
+++
@@ -1,9 +0,0 @@
-// /api/reset-machines.js
-import { redis } from "@/lib/redis";
-
-export default async function handler(req, res) {
-	if (req.method !== "POST") return res.status(405).end();
-
-	await redis.set("machines", []);
-	return res.status(200).json({ message: "Machines list reset" });
-}
